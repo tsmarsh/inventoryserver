3baa281edfdb957e3191383ba0b6df6706b42ba4
Tom Marsh <ts.marsh@gmail.com>
2014-04-08 18:03:58 -0400

    Basic servlet support is working
    First time in 3 weeks I have an app that would work if you turned it on.
    
    Still missing a list feature. I also suspect that I need to work on a 'head' feature for users.

---
+++ src/main/java/com/tailoredshapes/inventoryserver/serialisers/UserStringSerialiser.java
@@ -0,0 +1,39 @@
+package com.tailoredshapes.inventoryserver.serialisers;
+
+import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
+import com.tailoredshapes.inventoryserver.model.Inventory;
+import com.tailoredshapes.inventoryserver.model.User;
+import com.tailoredshapes.inventoryserver.urlbuilders.UrlBuilder;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import javax.inject.Inject;
+
+public class UserStringSerialiser implements Serialiser<User, String> {
+
+    private UrlBuilder<User> urlBuilder;
+    private final Serialiser<Inventory, String> inventorySerialiser;
+
+    @Inject
+    public UserStringSerialiser(UrlBuilder<User> urlBuilder, Serialiser<Inventory, String> inventorySerialiser) {
+        this.urlBuilder = urlBuilder;
+        this.inventorySerialiser = inventorySerialiser;
+    }
+
+    @Override
+    public String serialise(User user) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("id", urlBuilder.build(user));
+        jsonObject.put("name", user.getName());
+        jsonObject.put("publicKey", Base64.encode(user.getPublicKey().getEncoded()));
+
+        JSONArray inventories = new JSONArray();
+        for (Inventory inventory : user.getInventories()) {
+            inventories.put(new JSONObject(inventorySerialiser.serialise(inventory)));
+        }
+
+        jsonObject.put("inventories", inventories);
+
+        return jsonObject.toString();
+    }
+}
