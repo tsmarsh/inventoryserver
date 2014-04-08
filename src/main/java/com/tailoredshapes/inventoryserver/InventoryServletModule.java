9c5d506b24cf2548d24e2d7475da0ef23265f653
Tom Marsh <ts.marsh@gmail.com>
2014-04-08 18:18:53 -0400

    Pruning unused files
    Moved SimpleScope to test where it belongs too.

--- src/main/java/com/tailoredshapes/inventoryserver/InventoryServletModule.java
+++ src/main/java/com/tailoredshapes/inventoryserver/InventoryServletModule.java
@@ -7,7 +7,6 @@ import com.google.inject.Singleton;
 import com.google.inject.name.Names;
 import com.google.inject.persist.PersistFilter;
 import com.google.inject.persist.jpa.JpaPersistModule;
-import com.google.inject.servlet.RequestScoped;
 import com.google.inject.servlet.ServletModule;
 import com.google.inject.servlet.ServletScopes;
 import com.tailoredshapes.inventoryserver.dao.DAO;
@@ -18,16 +17,10 @@ import com.tailoredshapes.inventoryserver.model.User;
 import com.tailoredshapes.inventoryserver.parsers.Parser;
 import com.tailoredshapes.inventoryserver.repositories.Repository;
 import com.tailoredshapes.inventoryserver.responders.Responder;
-import com.tailoredshapes.inventoryserver.scopes.SimpleScope;
 import com.tailoredshapes.inventoryserver.servlets.Pestlet;
 import com.tailoredshapes.inventoryserver.urlbuilders.UrlBuilder;
 
 import javax.inject.Named;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 public class InventoryServletModule extends ServletModule {
     @Override
@@ -42,20 +35,6 @@ public class InventoryServletModule extends ServletModule {
         serveRegex("/users(/-?\\d+)?$").with(new Key<Pestlet<User>>() {});
         filterRegex("/users(/-?\\d+)?$").through(new Key<TFilter<User>>() {});
 
-        serve("/*").with(new HttpServlet() {
-            @Override
-            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                super.doGet(req, resp);
-                resp.sendError(404, "Cannot serve: " + req.getPathInfo());
-
-            }
-
-            @Override
-            protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                super.doPost(req, resp);
-                resp.sendError(404, "Cannot serve: " + req.getPathInfo());
-            }
-        });
         bind(Key.get(User.class, Names.named("current_user"))).to(User.class).in(ServletScopes.REQUEST);
         bind(Key.get(Inventory.class, Names.named("current_inventory"))).to(Inventory.class).in(ServletScopes.REQUEST);
     }
--- src/main/java/com/tailoredshapes/inventoryserver/handlers/HttpMethod.java
+++
@@ -1,5 +0,0 @@
-package com.tailoredshapes.inventoryserver.handlers;
-
-public enum HttpMethod {
-    get, post, put, delete
-}
