9774c605758a8db1d71b6b042ea6c5abe0097fd3
Tom Marsh <ts.marsh@gmail.com>
2014-04-25 23:46:00 -0400

    Now works with in memory gubbins too

--- src/test/java/com/tailoredshapes/inventoryserver/repositories/hibernate/HibernateCategoryRepositoryTest.java
+++ src/test/java/com/tailoredshapes/inventoryserver/repositories/hibernate/HibernateCategoryRepositoryTest.java
@@ -3,6 +3,7 @@ package com.tailoredshapes.inventoryserver.repositories.hibernate;
 import com.google.inject.Injector;
 import com.google.inject.Key;
 import com.google.inject.name.Names;
+import com.tailoredshapes.inventoryserver.GuiceTest;
 import com.tailoredshapes.inventoryserver.dao.DAO;
 import com.tailoredshapes.inventoryserver.model.Category;
 import com.tailoredshapes.inventoryserver.model.Inventory;
@@ -18,6 +19,9 @@ import org.junit.Test;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 
+import java.util.Map;
+
+import static com.tailoredshapes.inventoryserver.GuiceTest.*;
 import static com.tailoredshapes.inventoryserver.HibernateTest.hibernateInjector;
 import static org.junit.Assert.assertEquals;
 
@@ -25,12 +29,27 @@ public class HibernateCategoryRepositoryTest {
     private SimpleScope scope;
 
     @Test
+    public void memoryFindByName() throws Exception {
+        Repository<Category, Map<Long, Category>> repo = injector.getInstance(new Key<Repository<Category, Map<Long, Category>>>(){});
+        FinderFactory<Category, String, Map<Long, Category>> findByFullName= injector.getInstance(new Key<FinderFactory<Category, String, Map<Long, Category>>>(){});
+
+        testFindByName(injector, repo, findByFullName, new Runnable() {
+            @Override
+            public void run() {
+            }
+        });
+    }
+
+    @Test
     public void hibernateFindByName() throws Exception {
         final EntityManager manager = hibernateInjector.getInstance(EntityManager.class);
         EntityTransaction transaction = manager.getTransaction();
         transaction.begin();
 
-        testFindByName(hibernateInjector, new Runnable() {
+        Repository<Category, EntityManager> repo = hibernateInjector.getInstance(new Key<Repository<Category, EntityManager>>(){});
+        FinderFactory<Category, String, EntityManager> findByFullName= hibernateInjector.getInstance(new Key<FinderFactory<Category, String, EntityManager>>(){});
+
+        testFindByName(hibernateInjector, repo, findByFullName, new Runnable() {
             @Override
             public void run() {
                 manager.flush();
@@ -41,19 +60,16 @@ public class HibernateCategoryRepositoryTest {
         transaction.rollback();
     }
 
-    public void testFindByName(Injector injector, Runnable reset) throws Exception {
+    public <T> void testFindByName(Injector injector,Repository<Category, T> repo, FinderFactory<Category, String, T> findByFullName, Runnable reset) throws Exception {
         scope = injector.getInstance(SimpleScope.class);
         scope.enter();
         scope.seed(Key.get(User.class, Names.named("current_user")), new User());
 
         Category category = new CategoryBuilder().build();
         DAO<Category> dao = injector.getInstance(new Key<DAO<Category>>() {});
-        Repository<Category, EntityManager> repo = injector.getInstance(new Key<Repository<Category, EntityManager>>(){});
         Category savedCategory = dao.create(category);
 
         reset.run();
-
-        FinderFactory<Category, String, EntityManager> findByFullName= injector.getInstance(new Key<FinderFactory<Category, String, EntityManager>>(){});
 
         Category byId = repo.findBy(findByFullName.lookFor(savedCategory.getFullname()));
         assertEquals(savedCategory, byId);
@@ -73,13 +89,27 @@ public class HibernateCategoryRepositoryTest {
         Repository<Category, EntityManager> repo = hibernateInjector.getInstance(new Key<Repository<Category, EntityManager>>(){});
         FinderFactory<Category, String, EntityManager> findByFullName= hibernateInjector.getInstance(new Key<FinderFactory<Category, String, EntityManager>>(){});
 
-        testMissByName(hibernateInjector, repo, findByFullName);
+        testMissByName(repo, findByFullName);
 
         transaction.rollback();
         scope.exit();
     }
 
-    public <T> void testMissByName(Injector injector, Repository<Category, T> repo, FinderFactory<Category, String, T> findByFullName ) throws Exception {
+
+    @Test
+    public void memoryMissByName() throws Exception {
+        scope = injector.getInstance(SimpleScope.class);
+        scope.enter();
+
+        Repository<Category, Map<Long, Category>> repo = injector.getInstance(new Key<Repository<Category, Map<Long, Category>>>(){});
+        FinderFactory<Category, String, Map<Long, Category>> findByFullName= injector.getInstance(new Key<FinderFactory<Category, String, Map<Long, Category>>>(){});
+
+        testMissByName(repo, findByFullName);
+
+        scope.exit();
+    }
+
+    public <T> void testMissByName(Repository<Category, T> repo, FinderFactory<Category, String, T> findByFullName ) throws Exception {
         scope.seed(Key.get(User.class, Names.named("current_user")), new User());
 
         Category byId = repo.findBy(findByFullName.lookFor("brian"));
