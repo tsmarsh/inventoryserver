f95bc734caa861f814cb6badc4d159903f77bf8c
Tom Marsh <ts.marsh@gmail.com>
2014-05-18 07:08:03 -0400

    Stops IntelliJ complaining

--- src/main/java/com/tailoredshapes/inventoryserver/modules/RoutesModule.java
+++ src/main/java/com/tailoredshapes/inventoryserver/modules/RoutesModule.java
@@ -1,9 +1,11 @@
 package com.tailoredshapes.inventoryserver.modules;
 
 import com.google.inject.Key;
+import com.google.inject.Provides;
 import com.google.inject.TypeLiteral;
 import com.google.inject.name.Names;
 import com.google.inject.persist.PersistFilter;
+import com.google.inject.servlet.RequestScoped;
 import com.google.inject.servlet.ServletModule;
 import com.google.inject.servlet.ServletScopes;
 import com.tailoredshapes.inventoryserver.filters.TFilter;
@@ -11,6 +13,9 @@ import com.tailoredshapes.inventoryserver.filters.TransactionFilter;
 import com.tailoredshapes.inventoryserver.model.Inventory;
 import com.tailoredshapes.inventoryserver.model.User;
 import com.tailoredshapes.inventoryserver.servlets.Pestlet;
+
+import javax.inject.Named;
+import javax.inject.Scope;
 
 public class RoutesModule extends ServletModule {
     private final boolean usesPersistence;
@@ -50,11 +55,19 @@ public class RoutesModule extends ServletModule {
                 .through(Key.get(new TypeLiteral<TFilter<String, User, ?>>() {}, Names.named("user_root")));
         filterRegex("/users$")
                 .through(Key.get(new TypeLiteral<TFilter<Long, User, ?>>() {}, Names.named("user_root")));
+    }
 
-        bind(Key.get(User.class, Names.named("current_user")))
-                .to(User.class).in(ServletScopes.REQUEST);
+    @Provides
+    @Named("current_user")
+    @RequestScoped
+    public User providesCurrentUser(){
+        return new User();
+    }
 
-        bind(Key.get(Inventory.class, Names.named("current_inventory")))
-                .to(Inventory.class).in(ServletScopes.REQUEST);
+    @Provides
+    @Named("current_inventory")
+    @RequestScoped
+    public Inventory providesCurrentInventory(){
+        return new Inventory();
     }
 }
