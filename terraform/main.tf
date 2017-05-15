082eb56c01022e17c19b5d54390f4a871b1d14b8
Tom Marsh <tom.marsh@novartis.com>
2017-05-15 09:12:01 -0400

    Well plan works

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,23 +1,45 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-variable env {
-  type = string
-  description = "suffix for names, e.g. 'prod'"
-}
-
 resource "aws_ecr_repository" "docker_repo" {
-  name = "invserver-${env}"
+  name = "memserver"
 }
 
-resource "aws_ecs_cluster" "cluster" {
-  name = "invserver-${env}"
-
+resource "aws_ecs_cluster" "memserver" {
+  name = "memserver-${var.env}"
 }
 
-resource "aws_ecs_task_definition"{
+data "aws_ecs_task_definition" "memserver" {
+  task_definition = "${aws_ecs_task_definition.memserver.family}"
+}
 
-  container_definitions = ""
-  family = ""
+
+resource "aws_ecs_task_definition" "memserver" {
+  family = "memserver"
+
+  container_definitions = <<DEFINITION
+[
+  {
+    "cpu": 128,
+    "essential": true,
+    "image": "memserver:${var.env}",
+    "memory": 256,
+    "memoryReservation": 256,
+    "name": "memserver",
+    "portMappings": [
+        {
+          "hostPort": 80,
+          "containerPort": 1414,
+          "protocol": "tcp"
+        }
+    ]
+  }
+]
+DEFINITION
+}
+
+resource "aws_ecs_service" "memserver" {
+  name = "memserver"
+  cluster = "${aws_ecs_cluster.memserver.id}"
+  desired_count = 2
+
+  # Track the latest ACTIVE revision
+  task_definition = "${aws_ecs_task_definition.memserver.family}:${max("${aws_ecs_task_definition.memserver.revision}", "${data.aws_ecs_task_definition.memserver.revision}")}"
 }
